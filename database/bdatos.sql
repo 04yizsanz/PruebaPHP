bdbb876ef3e035361d7eb90c81918c9e7bb9411a
yizel_sanchez <sanchezyizel601@gmail.com>
2025-08-29 20:34:12 -0500

    actualizacion de base de datos t_productos

--- database/bdatos.sql
+++ database/bdatos.sql
@@ -15,3 +15,25 @@ CREATE TABLE IF NOT EXISTS  t_usuarios (
 )ENGINE=InnoDB;
 
 INSERT INTO t_usuarios VALUE(NULL, 'Yizel','Sanchez','yizelsancas2000@gmail.com','123456,','admin','imagen');
+
+CREATE TABLE IF NOT EXISTS t_categorias(
+    id_categoria           int(11)      auto_increment NOT NULL,
+    nombre_categoria       varchar(100)               NOT NULL,
+    CONSTRAINT pk_categoria         PRIMARY KEY (id_categoria)
+)ENGINE=InnoDB;
+
+INSERT INTO t_categorias VALUE(NULL, 'Construccion'), (NULL, 'Electrico'), (NULL, 'Pinturas'), (NULL, 'Pisos');
+
+CREATE TABLE IF NOT EXISTS t_productos(
+    id_producto        int(11)      auto_increment NOT NULL,
+    nombre_producto    varchar(100)                NOT NULL,
+    descripcion        varchar(200)                NOT NULL,
+    precio             float(10,9)                 NOT NULL,
+    stock              int(100)                    NOT NULL,
+    oferta             varchar(200)                NOT NULL,
+    fecha              date                        NOT NULL,
+    imagen             varchar(255)                NOT NULL,
+    id_categoria       int(11)                     NOT NULL,
+    CONSTRAINT pk_producto       PRIMARY KEY (id_producto),
+    CONSTRAINT fk_categoria     FOREIGN KEY (id_categoria) REFERENCES t_categorias(id_categoria)
+)ENGINE=InnoDB;
