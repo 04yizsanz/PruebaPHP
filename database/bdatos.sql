16468bd762e4680773cc813dc086c89fb6340233
yizel_sanchez <sanchezyizel601@gmail.com>
2025-09-01 19:58:25 -0500

    base de datos terminada

--- database/bdatos.sql
+++ database/bdatos.sql
@@ -10,16 +10,16 @@ CREATE TABLE IF NOT EXISTS  t_usuarios (
     password    varchar(200)               NOT NULL,
     rol         varchar(20)                NOT NULL,
     imagen      varchar(200),    
-    CONSTRAINT pk_usuario   PRIMARY KEY (id_usuario),     
-    CONSTRAINT uq_email      UNIQUE (email)      
+    CONSTRAINT  pk_usuario                 PRIMARY KEY (id_usuario),     
+    CONSTRAINT  uq_email                   UNIQUE      (email)      
 )ENGINE=InnoDB;
 
 INSERT INTO t_usuarios VALUE(NULL, 'Yizel','Sanchez','yizelsancas2000@gmail.com','123456,','admin','imagen');
 
 CREATE TABLE IF NOT EXISTS t_categorias(
     id_categoria           int(11)      auto_increment NOT NULL,
-    nombre_categoria       varchar(100)               NOT NULL,
-    CONSTRAINT pk_categoria         PRIMARY KEY (id_categoria)
+    nombre_categoria       varchar(100)                NOT NULL,
+    CONSTRAINT             pk_categoria                PRIMARY KEY (id_categoria)
 )ENGINE=InnoDB;
 
 INSERT INTO t_categorias VALUE(NULL, 'Construccion'), (NULL, 'Electrico'), (NULL, 'Pinturas'), (NULL, 'Pisos');
@@ -34,6 +34,29 @@ CREATE TABLE IF NOT EXISTS t_productos(
     fecha              date                        NOT NULL,
     imagen             varchar(255)                NOT NULL,
     id_categoria       int(11)                     NOT NULL,
-    CONSTRAINT pk_producto       PRIMARY KEY (id_producto),
-    CONSTRAINT fk_categoria     FOREIGN KEY (id_categoria) REFERENCES t_categorias(id_categoria)
+    CONSTRAINT         pk_producto                 PRIMARY KEY (id_producto),
+    CONSTRAINT         fk_categoria                FOREIGN KEY (id_categoria) REFERENCES t_categorias(id_categoria)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS t_pedidos(
+    id_pedido          int(11)      auto_increment NOT NULL,
+    id_usuario         int(11)                     NOT NULL,
+    fecha              date                        NOT NULL,
+    hora               time                        NOT NULL,
+    ciudad             varchar(100)                NOT NULL,
+    direccion          varchar(200)                NOT NULL,
+    costo              float(10,2)                 NOT NULL,
+    estado             varchar(20)                 NOT NULL,
+    CONSTRAINT         pk_pedido                   PRIMARY KEY (id_pedido),
+    CONSTRAINT         fk_usuario                  FOREIGN KEY (id_usuario) REFERENCES t_usuarios(id_usuario)
+)ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS t_detallePedidos(
+    id_detallePedido   int(11)      auto_increment NOT NULL,
+    id_pedido          int(11)                     NOT NULL,
+    id_producto        int(11)                     NOT NULL,
+    unidades           int(100)                    NOT NULL,
+    CONSTRAINT         pk_detallePedido            PRIMARY KEY (id_detallePedido),
+    CONSTRAINT         fk_pedido                   FOREIGN KEY (id_pedido)   REFERENCES t_pedidos(id_pedido),
+    CONSTRAINT         fk_producto                 FOREIGN KEY (id_producto) REFERENCES t_productos(id_producto)  
 )ENGINE=InnoDB;
